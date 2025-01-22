bdd7dd4154c3c3c83bea907933d942708f193dce
faysalmohammed2020 <faysalmohammed.shah@gmail.com>
2025-01-22 16:21:14 +0600

    login updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,9 +7,7 @@ import Image from "next/image";
 //import { serviceBlogTitle } from "@/app/data/blogData";
 import { postdata } from "@/app/(main)/data/postdata";
 
-
-
-  const HeaderMenu: React.FC = () => {
+const HeaderMenu: React.FC = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState<string>("");
 
@@ -45,7 +43,11 @@ import { postdata } from "@/app/(main)/data/postdata";
             onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
             className="text-black"
           >
-            {isMobileMenuOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
+            {isMobileMenuOpen ? (
+              <X className="w-6 h-6" />
+            ) : (
+              <Menu className="w-6 h-6" />
+            )}
           </button>
         </div>
 
@@ -60,54 +62,60 @@ import { postdata } from "@/app/(main)/data/postdata";
 
           {/* Services Dropdown */}
           <li className="group relative">
-          <div className="flex items-center cursor-pointer hover:text-orange-400">
-           <span>Services</span>
-           <ChevronDown className="ml-2 w-4 h-4" />
-          </div>
-          <ul className="absolute z-50 left-0 mt-2 w-64 bg-gray-700 text-white shadow-lg rounded-md opacity-0 group-hover:opacity-100 group-hover:visible transition-all duration-300 ease-in-out invisible h-auto">
-          <li className="px-4 py-2 hover:bg-gray-900">
-          <Link href="/services/long-distance-moving">Long Distance Moving</Link>
+            <div className="flex items-center cursor-pointer hover:text-orange-400">
+              <span>Services</span>
+              <ChevronDown className="ml-2 w-4 h-4" />
+            </div>
+            <ul className="absolute z-50 left-0 mt-2 w-64 bg-gray-700 text-white shadow-lg rounded-md opacity-0 group-hover:opacity-100 group-hover:visible transition-all duration-300 ease-in-out invisible h-auto">
+              <li className="px-4 py-2 hover:bg-gray-900">
+                <Link href="/services/long-distance-moving">
+                  Long Distance Moving
+                </Link>
+              </li>
+              <li className="px-4 py-2 hover:bg-gray-900">
+                <Link href="/services/auto-transport">Auto Transport</Link>
+              </li>
+              <li className="px-4 py-2 hover:bg-gray-900">
+                <Link href="/services/storage-solutions">
+                  Storage Solutions
+                </Link>
+              </li>
+              <li className="group relative px-4 py-2 hover:bg-gray-900">
+                <div className="flex items-center cursor-pointer">
+                  <span className="hover:text-orange-400">
+                    Commercial Moving
+                  </span>
+                  <ChevronDown className="ml-2 w-4 h-4" />
+                </div>
+                {/* Nested Dropdown */}
+                <ul className="absolute z-50 left-full top-0 mt-0 w-64 bg-gray-700 text-white shadow-lg rounded-md opacity-0 group-hover:opacity-100 group-hover:visible transition-all duration-300 ease-in-out invisible">
+                  <li className="px-4 py-2 hover:bg-gray-900">
+                    <Link href="/services/commercial-moving/office-relocation">
+                      Office Relocation
+                    </Link>
+                  </li>
+                  <li className="px-4 py-2 hover:bg-gray-900">
+                    <Link href="/services/commercial-moving/retail-relocation">
+                      Retail Relocation
+                    </Link>
+                  </li>
+                  <li className="px-4 py-2 hover:bg-gray-900">
+                    <Link href="/services/commercial-moving/corporate-relocation">
+                      Corporate Relocation
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li className="px-4 py-2 hover:bg-gray-900">
+                <Link href="/services/specialized-moving">
+                  Specialized Moving
+                </Link>
+              </li>
+              <li className="px-4 py-2 hover:bg-gray-900">
+                <Link href="/services/small-moves">Small Moves</Link>
+              </li>
+            </ul>
           </li>
-          <li className="px-4 py-2 hover:bg-gray-900">
-          <Link href="/services/auto-transport">Auto Transport</Link>
-        </li>
-          <li className="px-4 py-2 hover:bg-gray-900">
-          <Link href="/services/storage-solutions">Storage Solutions</Link>
-        </li>
-        <li className="group relative px-4 py-2 hover:bg-gray-900">
-        <div className="flex items-center cursor-pointer">
-        <span className="hover:text-orange-400">Commercial Moving</span>
-        <ChevronDown className="ml-2 w-4 h-4" />
-      </div>
-      {/* Nested Dropdown */}
-      <ul className="absolute z-50 left-full top-0 mt-0 w-64 bg-gray-700 text-white shadow-lg rounded-md opacity-0 group-hover:opacity-100 group-hover:visible transition-all duration-300 ease-in-out invisible">
-        <li className="px-4 py-2 hover:bg-gray-900">
-          <Link href="/services/commercial-moving/office-relocation">
-            Office Relocation
-          </Link>
-        </li>
-        <li className="px-4 py-2 hover:bg-gray-900">
-          <Link href="/services/commercial-moving/retail-relocation">
-            Retail Relocation
-          </Link>
-        </li>
-        <li className="px-4 py-2 hover:bg-gray-900">
-          <Link href="/services/commercial-moving/corporate-relocation">
-            Corporate Relocation
-          </Link>
-        </li>
-      </ul>
-    </li>
-    <li className="px-4 py-2 hover:bg-gray-900">
-      <Link href="/services/specialized-moving">Specialized Moving</Link>
-    </li>
-    <li className="px-4 py-2 hover:bg-gray-900">
-      <Link href="/services/small-moves">Small Moves</Link>
-    </li>
-  </ul>
-  </li>
-
-
 
           {/* About Us Dropdown */}
           <li className="group relative">
@@ -131,58 +139,67 @@ import { postdata } from "@/app/(main)/data/postdata";
 
           {/* Blog Dropdown */}
           <li className="group relative">
-          <div className="flex items-center cursor-pointer hover:text-orange-500">
-          <span className="text-lg">Blog</span>
-          <ChevronDown className="ml-2 w-5 h-5 text-gray-300 transition-all duration-300 ease-in-out group-hover:rotate-180" />
-          </div>
-
-          <ul className="absolute z-50 left-0 mt-4 bg-gray-800 text-white shadow-lg rounded-lg opacity-0 group-hover:opacity-100 group-hover:visible transition-all duration-300 ease-in-out invisible w-96 p-4">
-          <div className="bg-orange-600 w-32 p-2 rounded-full text-white text-sm font-semibold">
-            Total Blogs: {filteredBlogs.length}
-          </div>
-
-    {/* Search Input */}
-    <div className="mt-4 relative">
-  <div className="absolute inset-y-0 left-3 flex items-center pointer-events-none">
-    <span className="text-gray-400 text-lg">🔍</span>
-  </div>
-  <input
-    type="text"
-    placeholder="Search blogs..."
-    value={searchQuery}
-    onChange={(e) => setSearchQuery(e.target.value)}
-    className="w-80 p-3 pl-12 rounded-md bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-orange-500 shadow-sm border border-gray-600 transition-all duration-300"
-  />
-</div>
-
-    <div className="scrollbar mt-4 space-y-4 ">
-      {filteredBlogs.length > 0 ? (
-        filteredBlogs.map((id) => (
-          <li
-            key={id.ID}
-            className="group p-2 rounded-md bg-slate-700 hover:from-orange-500 hover:to-orange-400 transition-colors duration-300 ease-in-out shadow-md"
-          >
-            <Link
-              href={`/blogs/${id.ID}`}
-              className="text-sm sm:text-base font-medium text-gray-100 hover:underline hover:text-orange-600"
-            >
-              {/* Highlight Matching Letters */}
-              <span
-                dangerouslySetInnerHTML={{
-                  __html: highlightSearchTerm(id.post_title, searchQuery),
-                }}
-              />
-            </Link>
+            <div className="flex items-center cursor-pointer hover:text-orange-500">
+              <span className="text-lg">Blog</span>
+              <ChevronDown className="ml-2 w-5 h-5 text-gray-300 transition-all duration-300 ease-in-out group-hover:rotate-180" />
+            </div>
+            <ul className="absolute z-50 left-0 mt-4 bg-gray-800 text-white shadow-lg rounded-lg opacity-0 group-hover:opacity-100 group-hover:visible transition-all duration-300 ease-in-out invisible w-96 p-4">
+              <div className="bg-orange-600 w-32 p-2 rounded-full text-white text-sm font-semibold">
+                Total Blogs: {filteredBlogs.length}
+              </div>
+              {/* Search Input */}
+              <div className="mt-4 relative">
+                <div className="absolute inset-y-0 left-3 flex items-center pointer-events-none">
+                  <span className="text-gray-400 text-lg">🔍</span>
+                </div>
+                <input
+                  type="text"
+                  placeholder="Search blogs..."
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                  className="w-80 p-3 pl-12 rounded-md bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-orange-500 shadow-sm border border-gray-600 transition-all duration-300"
+                />
+              </div>
+              <div className="scrollbar mt-4 space-y-4">
+                {filteredBlogs.length > 0 ? (
+                  filteredBlogs.map((id) => (
+                    <li
+                      key={id.ID}
+                      className="group p-2 rounded-md bg-slate-700 hover:from-orange-500 hover:to-orange-400 transition-colors duration-300 ease-in-out shadow-md"
+                    >
+                      <Link
+                        href={`/blogs/${id.ID}`}
+                        className="text-sm sm:text-base font-medium text-gray-100 hover:underline hover:text-orange-600"
+                      >
+                        {/* Highlight Matching Letters */}
+                        <span
+                          dangerouslySetInnerHTML={{
+                            __html: highlightSearchTerm(
+                              id.post_title,
+                              searchQuery
+                            ),
+                          }}
+                        />
+                      </Link>
+                    </li>
+                  ))
+                ) : (
+                  <p className="text-gray-300 text-sm">No blogs found.</p>
+                )}
+              </div>
+            </ul>
           </li>
-        ))
-      ) : (
-        <p className="text-gray-300 text-sm">No blogs found.</p>
-      )}
-    </div>
-  </ul>
-</li>
 
+          
+          
         </ul>
+        {/* Sign In Button */}
+        <Link
+              href="/AdminLogin"
+              className=" bg-orange-500 text-white px-6 py-2 rounded-full hover:bg-orange-600 transition-all duration-300"
+            >
+              Admin Login 
+            </Link>
 
         {/* Mobile Menu */}
         {isMobileMenuOpen && (
