e691401c71268aa10b16136fc5151be540689551
faysalmohammed2020 <faysalmohammed.shah@gmail.com>
2025-12-22 17:03:30 +0600

    build error fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Providers } from "./providers";
 import Script from "next/script";
 import AnalyticsTracker from "@/components/AnalyticsTracker";
+import { Suspense } from "react";
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
@@ -31,7 +32,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </Script>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+         <Suspense fallback={null}>
          <AnalyticsTracker />
+         </Suspense>
         <Providers>{children}</Providers>
       </body>
     </html>
